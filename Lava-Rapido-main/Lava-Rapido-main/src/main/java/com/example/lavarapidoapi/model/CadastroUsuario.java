b0008077c979a9cd0a6cc6386b3a0c59dd51a983
Matheus Souza <125572561+matheusroberto04@users.noreply.github.com>
2024-03-14 09:30:11 -0300

    Update Lava Rapido
    Update

--- Lava-Rapido-main/Lava-Rapido-main/src/main/java/com/example/lavarapidoapi/model/CadastroUsuario.java
+++ Lava-Rapido-main/Lava-Rapido-main/src/main/java/com/example/lavarapidoapi/model/CadastroUsuario.java
@@ -1,7 +1,6 @@
 package com.example.lavarapidoapi.model;
 
-import org.springframework.data.annotation.Id;
-
+import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
